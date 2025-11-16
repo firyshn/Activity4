49f246aa16d9bac13b9f97d56954438ced2ff029
firyshn <shennamaebellen@gmail.com>
2025-11-16 14:26:34 +0800

    feat/create-table-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -37,3 +37,10 @@ CREATE TABLE doctors (
     phone VARCHAR (15)
     email VARCHAR (100)
 );
+
+CREATE TABLE invoices (
+    invoiceid PRIMARY KEY
+    appointid FOREIGN KEY
+    totalamount NUMERIC(10,2)
+    paymentdate TIME
+); 
