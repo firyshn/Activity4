d085e6437e00576eafde56cf607260fe1385155f
firyshn <shennamaebellen@gmail.com>
2025-11-16 14:54:26 +0800

    feat/insert-10-rows-owners

--- veterinary.sql
+++ veterinary.sql
@@ -3,7 +3,7 @@ CREATE DATABASE veterinary;
 CREATE TABLE owners(
     ownerid INT PRIMARY KEY,
     ofirstname VARCHAR(50),
-    olastname VARCHAR(50),
+    oflastname VARCHAR(50),
     address VARCHAR(100),
     phone VARCHAR(15),
     email VARCHAR(100)
@@ -56,3 +56,16 @@ CREATE TABLE medicalrecords  (
     FOREIGN KEY (animalid) REFERENCES animals (animalid),
     FOREIGN KEY (doctorid) REFERENCES doctors (doctorid)
 );
+
+INSERT INTO owners(ownerid, ofirstname, oflastname, address, phone, email)
+VALUES 
+(1, 'Hannah', 'Cuevas', 'Cuenca', 09944673669, 'hannah@gmail.com'),
+(2, 'Gee-Anne', 'Lescano', 'Lipa', 09924673869, 'geeanne@gmail.com'),
+(3, 'Jim', 'Ebreo', 'Rosario', 09934673760, 'jimebreo@gmail.com'),
+(4, 'Shenna', 'Bellen', 'Rosario', 09910056722, 'shennabellen@gmail.com'),
+(5, 'Mark', 'Maglaway', 'Cuenca', 09123456789, 'markmaglaway@gmail.com'),
+(6, 'John', 'Cuevas', 'Mindanao', 09568926936, 'johncuevas@gmail.com'),
+(7, 'Spongebob', 'Squarepants', 'Bikini Bottom', 09936275382, 'spongebob@gmail.com'),
+(8, 'Dora', 'Explorer', 'Japan', 09926783528, 'doraexplorer@gmail.com'),
+(9, 'Angel', 'Vargas', 'Bulacan', 0991776722, 'angelvargas@gmail.com'),
+(10, 'Ariel', 'Mermaid', 'San Juan', 0987646789, 'arielmermaid@gmail.com');
